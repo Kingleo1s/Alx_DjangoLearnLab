c7838ad0759e5f264a418673eab088d03cac0154
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-04 15:51:50 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,30 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 
-# Helpers: check user role
-def is_admin(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
-
+# Role check helper
 def is_librarian(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
 
-def is_member(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
 
-
-# Admin view
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, "relationship_app/admin_view.html")
-
-
-# Librarian view
+# Librarian-only view
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
-
-
-# Member view
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, "relationship_app/member_view.html")

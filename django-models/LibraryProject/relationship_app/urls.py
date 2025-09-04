02bdfb8050251add23420ac972c7fd7bec4465c2
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-04 15:46:01 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views   # this makes "views.register" available
+from . import views
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # FBV
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
     # authentication
-    path("register/", views.register, name="register"),  # <-- checker wants "views.register"
+    path("register/", views.register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
+    # role-based views
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
 ]

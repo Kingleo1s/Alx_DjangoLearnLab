8c77111d27d63b9007a1eca16575b3eb09de1875
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-04 16:02:31 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,3 +14,8 @@ urlpatterns = [
     # role-based views
     path("librarian-view/", views.librarian_view, name="librarian_view"),
 ]
+urlpatterns = [
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/edit/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", views.delete_book, name="delete_book"),
+]

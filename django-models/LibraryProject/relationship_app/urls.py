5270bcbaabeef9fd39debbbfde06cda5ac82a775
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-04 15:52:30 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,5 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
     # role-based views
-    path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
-    path("member-view/", views.member_view, name="member_view"),
 ]

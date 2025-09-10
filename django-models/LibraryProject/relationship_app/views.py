cfe7fa409b77764f747fe5e99f0d0430c1771598
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-10 11:57:42 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Book   
-from django.views.generic import DetailView
 from .models import Library
 from django.views.generic.detail import DetailView
 

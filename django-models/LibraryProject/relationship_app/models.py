aed92dda874095b9424854960f8d6ea436201d43
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-10 12:44:48 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,8 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Author -> Book (One-to-Many with ForeignKey)
 class Author(models.Model):
@@ -32,3 +36,25 @@ class Librarian(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.library.name})"
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+# ✅ Automatically create UserProfile on user creation
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role="Member")  # default role
+

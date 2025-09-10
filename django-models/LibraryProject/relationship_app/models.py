e9aaaf9c172a36009bd92cbfbce6d77df3d8aeae
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-10 12:55:44 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,11 +13,20 @@ class Author(models.Model):
 
 
 class Book(models.Model):
+   class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.title} by {self.author.name}"
+        return f"{self.title} by {self.author} ({self.publication_year})"
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 
 # Library -> Book (Many-to-Many)

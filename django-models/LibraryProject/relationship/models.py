47dfbdc11e9cd6965e597514247dd80a401cdee0
kingleo1s <emmanuelogbodo44@gmail.com>
2025-09-10 10:34:46 +0100

    new work

---
+++ django-models/LibraryProject/relationship/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Author -> Book (One-to-Many using ForeignKey)
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+
+# Library -> Book (Many-to-Many)
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+
+# Library -> Librarian (One-to-One)
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})"
+
+# Create your models here.

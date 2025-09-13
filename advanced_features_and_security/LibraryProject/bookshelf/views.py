62cb5567049463beed9b3e7f6fa50a80aa9e4e62
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-13 11:26:43 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+# Protect with permission check and raise_exception=True
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    # Query all books
+    books = Book.objects.all()
+    # Pass them into the template context
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+

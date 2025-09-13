2984ddc95d9f8a7934eb3a316eb8a5f3d959f033
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-13 11:06:31 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,11 +1,26 @@
 
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import models
 
-# Author -> Book (One-to-Many with ForeignKey)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -13,24 +28,6 @@ class Author(models.Model):
         return self.name
 
 
-class Book(models.Model):
-   class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
-
-# Library -> Book (Many-to-Many)
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name="libraries")
@@ -38,8 +35,6 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-
-# Library -> Librarian (One-to-One)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
@@ -63,24 +58,10 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(instance, created):
     if created:
         UserProfile.objects.create(user=instance, role="Member")  # default role
 
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateField()
 
-    class Meta:
-        permissions = [
-            ("can_view", "Can view book"),
-            ("can_create", "Can create book"),
-            ("can_edit", "Can edit book"),
-            ("can_delete", "Can delete book"),
-        ]
-
-    def __str__(self):
-        return self.title

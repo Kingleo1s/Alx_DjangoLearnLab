4375ed4462fa65a51a7af749a30c53966ca93f09
kingleo1s <emmanuelogbodo44@gmail.com>
2025-09-14 21:18:08 +0100

    working now

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("publication_year", "author")

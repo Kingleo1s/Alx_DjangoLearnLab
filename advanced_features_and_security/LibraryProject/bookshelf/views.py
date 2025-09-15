85d87312d1155cdb4142d4cc7f51ea310e98bc9a
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-15 21:55:39 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm, ExampleForm
 
 # Protect with permission check and raise_exception=True
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -16,3 +17,32 @@ def search_books(request):
     query = request.GET.get("q", "")
     books = Book.objects.filter(title__icontains=query)  # ORM prevents SQL injection
     return render(request, "bookshelf/book_list.html", {"books": books})
+# LibraryProject/bookshelf/views.py
+
+
+
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+def example_view(request):
+    
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process form safely
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            return render(request, "bookshelf/form_example.html", {
+                "form": form,
+                "success": True,
+                "name": name,
+            })
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/form_example.html", {"form": form})

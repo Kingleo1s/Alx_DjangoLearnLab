2060982dd9b3b3ece3275acf91fb247f5f8822b2
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-15 23:08:10 +0100

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
-    'relationship_app',
-    'accounts',
-    'csp',
+    "bookshelf",
+    "relationship_app",
+    "accounts",
+    "csp",
 
 ]
 

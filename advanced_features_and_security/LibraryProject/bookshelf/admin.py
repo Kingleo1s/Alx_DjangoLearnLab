fa9fe20c2501c3965c8c19cd3d7abb145a2c684c
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-20 15:58:16 +0100

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -14,18 +14,15 @@ class BookAdmin(admin.ModelAdmin):
 class CustomUserAdmin(UserAdmin):
     
     list_display = ("username", "email", "first_name", "last_name", "date_of_birth", "is_staff")
-
-    
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
     )
-
-    
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
     )
 
 
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book, BookAdmin)
 
 

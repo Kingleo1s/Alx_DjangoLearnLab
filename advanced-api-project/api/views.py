c6648e462687cc6c66fd4272b8cbf11caf417ba0
kingleo1s <emmanuelogbodo44@gmail.com>
2025-09-26 15:46:53 +0100

    work

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from .models import Author
 from .serializers import AuthorSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly

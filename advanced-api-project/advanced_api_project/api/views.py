a9a99a602534b6414867867787d9002854496eff
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-09-26 13:29:48 +0100

    Update views.py

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from api.models import Author, Book
+from api.serializers import AuthorSerializer
 
-# Create your views here.
+a1 = Author.objects.create(name="George Orwell")
+Book.objects.create(title="1984", publication_year=1949, author=a1)
+Book.objects.create(title="Animal Farm", publication_year=1945, author=a1)
+
+serializer = AuthorSerializer(a1)
+print(serializer.data)
+# Output includes author details + nested books
+

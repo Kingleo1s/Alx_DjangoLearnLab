dd14a4cc5888d078e4de18d185056face6e95cd9
kingleo1s <emmanuelogbodo44@gmail.com>
2025-10-01 16:13:24 +0100

    project

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logged_out.html"), name="logout"),
     path("", views.home, name="home"),   # this makes {% url 'blog:home' %} work
     path('profile/', views.profile, name='profile'),
+
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),

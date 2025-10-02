97592e31f0063dd83d302921f88bdfd09baaabf8
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-10-02 11:30:40 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path("post/", views.post_list, name="post"),
 
-    path("posts/<int:post_pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("post/<int:post_>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
 

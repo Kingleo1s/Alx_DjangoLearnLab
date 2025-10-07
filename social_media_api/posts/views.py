a217ba913a3a6f8e11fbfa73840399bcc93c26b2
kingleo1s <emmanuelogbodo44@gmail.com>
2025-10-07 14:50:09 +0100

    new post app created

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,29 +1,24 @@
-from rest_framework import viewsets, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').prefetch_related('comments').all()
+    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['author__id']            # filter by author id
-    search_fields = ['title', 'content']        # search by title or content
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post').all()
+    queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['post', 'author']        # filter comments by post or author
-    search_fields = ['content']
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+ 
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)

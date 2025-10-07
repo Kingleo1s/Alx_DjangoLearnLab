a9662ff5dfb3bd8eef54734933f1af0832076e0c
kingleo1s <emmanuelogbodo44@gmail.com>
2025-10-07 14:39:49 +0100

    new post app created

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'rest_framework.authtoken',
+    'posts',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -63,9 +65,16 @@ REST_FRAMEWORK = {
 'rest_framework.authentication.TokenAuthentication',
 'rest_framework.authentication.SessionAuthentication',
 ),
+
 'DEFAULT_PERMISSION_CLASSES': (
 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 ),
+
+'DEFAULT_FILTER_BACKENDS': (
+    'django_filters.rest_framework.DjangoFilterBackend',
+    'rest_framework.filters.SearchFilter',
+    'rest_framework.filters.OrderingFilter',)
+
 }
 
 TEMPLATES = [
@@ -137,3 +146,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+

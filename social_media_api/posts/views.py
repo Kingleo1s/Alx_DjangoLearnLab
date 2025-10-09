70fdac44f3f7b0b4bd65b79fbf143e01112cf547
kingleo1s <emmanuelogbodo44@gmail.com>
2025-10-09 15:09:36 +0100

    posts views updated

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,24 +1,35 @@
-from rest_framework import viewsets, permissions
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # ← and this one
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
